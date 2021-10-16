ed557a1c0826fcf22f2de78b4ca90498554b283b
Vineet Kumar <vineet.kmr1708@gmail.com>
2021-10-16 13:01:42 +0530

    update

---
+++ whatsapp-mern/src/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Chat.css";
+
+function Chat() {
+    return (
+        <div className= "chat">
+            <h1>Hey i m the chat</h1>
+        </div>
+    )
+}
+
+export default Chat
---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBSpc4G_Y5K4KdlTMOjc9ogDQEcy8jQqro",
+    authDomain: "whatsapp-mern-98959.firebaseapp.com",
+    projectId: "whatsapp-mern-98959",
+    storageBucket: "whatsapp-mern-98959.appspot.com",
+    messagingSenderId: "1049401950089",
+    appId: "1:1049401950089:web:3745453941008385caab58"
+  };
