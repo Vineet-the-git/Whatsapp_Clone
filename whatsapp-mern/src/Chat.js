9568fdae9b6bffa68b57d8a65803eb4cba22c232
Vineet Kumar <vineet.kmr1708@gmail.com>
2021-10-18 12:11:27 +0530

    Update

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,12 +1,93 @@
+import MoreVert from '@mui/icons-material/MoreVert';
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
+import { Avatar, IconButton } from '@mui/material';
 import React from 'react';
 import "./Chat.css";
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import SelectInput from '@mui/material/Select/SelectInput';
+import MicIcon from '@mui/icons-material/Mic';
 
 function Chat() {
     return (
-        <div className= "chat">
-            <h1>Hey i m the chat</h1>
+        <div className="chat">
+            <div className="chat_header">
+                <Avatar />
+
+                <div className="chat_headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat_headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat_body">
+                <p className="chat_message">
+                    <span className="chat_name">
+                        Vineet
+                    </span>
+                    This is a message
+                    <span className="chat_timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat_reciever chat_message">
+                    <span className="chat_name">
+                        Vineet
+                    </span>
+                    This is a message
+                    <span className="chat_timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat_message">
+                    <span className="chat_name">
+                        Vineet
+                    </span>
+                    This is a message
+                    <span className="chat_timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat_message">
+                    <span className="chat_name">
+                        Vineet
+                    </span>
+                    This is a message
+                    <span className="chat_timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
+            <div className="chat_footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input
+                        placeholder="Type a message"
+                        type="text" />
+                    <button type="submit">
+                        Send a message
+                    </button>
+                </form>
+                <MicIcon/>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Chat
--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -3,24 +3,43 @@ import "./Sidebar.css";
 import ChatIcon from '@mui/icons-material/Chat';
 import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import {Avatar, IconButton } from '@mui/material';
+import { Avatar, IconButton } from '@mui/material';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import SearchOutlined from '@mui/icons-material/SearchOutlined';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar_header">
+                <Avatar src="https://avatars.githubusercontent.com/u/80666035?s=400&u=8beb83d2960c259d702f9f4296b900d1f135b101&v=4" />
                 <div className="sidebar_headerRight">
                     <IconButton>
-                    <DonutLargeIcon />
+                        <DonutLargeIcon />
                     </IconButton>
                     <IconButton>
-                    <ChatIcon />
+                        <ChatIcon />
                     </IconButton>
                     <IconButton>
-                    <MoreVertIcon />
+                        <MoreVertIcon />
                     </IconButton>
 
                 </div>
+
+            </div>
+
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
+            </div>
+
+            <div className="sidebar_chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
 
             </div>
 
